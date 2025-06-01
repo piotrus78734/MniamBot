9611214fcb431f926b8d379c1639af65a4cb6ea2
piotrus78734 <zuzkakluskak@gmail.com>
2025-06-01 16:13:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
-from PIL import ImageFont
 
+# Inicjalizacja I2C, domyślny adres 0x3C
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
 
 with canvas(device) as draw:
-    draw.text((10, 10), "Hello RPi!", fill=255)
+    draw.text((10, 10), "Hello OLED!", fill=255)
