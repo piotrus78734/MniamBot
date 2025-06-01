b8604ba00337181a815f0e979ca2ddc69aecb4ca
piotrus78734 <zuzkakluskak@gmail.com>
2025-06-01 16:22:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
+from PIL import Image, ImageDraw
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
-from luma.core.render import canvas
-import time  # ← potrzebne do pauzy
 
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
-
-device.clear()
 device.contrast(255)
+device.clear()
 
-with canvas(device) as draw:
-    draw.text((10, 10), "Hello OLED!", fill=255)
+image = Image.new("1", device.size)
+draw = ImageDraw.Draw(image)
+draw.text((10, 10), "Hello OLED!", fill=255)
 
-# Czekaj np. 10 sekund, żeby widzieć tekst
-time.sleep(10)
+device.display(image)
