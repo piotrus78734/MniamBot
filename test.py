aebcd930c7bf6b34127ccb866f6e67d3f71991c7
piotrus78734 <zuzkakluskak@gmail.com>
2025-06-01 16:19:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
 
-serial = i2c(port=1, address=0x3C)  # adres z i2cdetect
+serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial)
+
+device.clear()
+device.contrast(255)
 
 with canvas(device) as draw:
     draw.text((10, 10), "Hello OLED!", fill=255)
